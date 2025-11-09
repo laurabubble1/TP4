89d229b2d564b2fd593130d88f9746f7f0b4f462
GitHub Action <action@github.com>
2025-11-08 23:07:35 -0500

    good commit revision

--- bisect.py
+++ bisect.py
@@ -2,7 +2,7 @@ import os
 
 # Récupérer le hash du commit mauvais (HEAD)
 badhash = os.popen("git rev-parse HEAD").read().strip()
-# Récupérer le hash du commit bon (HEAD~10)
-goodhash = os.popen("git rev-parse HEAD~10").read().strip()
+
+goodhash = os.popen("git rev-list --max-parents=0 HEAD").read().strip()
 os.system(f"git bisect start {badhash} {goodhash}")
 os.system("git bisect run python manage.py test")
